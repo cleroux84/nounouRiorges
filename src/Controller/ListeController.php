4ed8b8e63ed958286371f7c6129257f676ab0ec3
cleroux <cleroux84@gmail.com>
2020-10-01 14:11:56 +0200

    query select par liste

--- src/Controller/ListeController.php
+++ src/Controller/ListeController.php
@@ -8,17 +8,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ListeController extends AbstractController
 {
+    
     /**
      * @Route("/liste", name="liste")
      */
     public function index()
     {
         $repo = $this->getDoctrine()->getRepository(Profils::class);
+        $listeUsers = $repo->findAll(); 
+        $listeUsersByPlaceDispo = $repo->findBy(array(), array('placeDispo'=>'desc'));  
+        $listeUsersByTarif = $repo->findBy(array(), array('tarifHoraire'=>'asc')); 
+        $listeUsersByAgrement = $repo->findBy(array(), array('AgrementDate'=>'asc')); 
 
-        $listeUsers = $repo->findAll();
         return $this->render('liste/index.html.twig', [
             'controller_name' => 'ListeController',
             'listeUsers' => $listeUsers,
+            'listeUserByPlaceDispo' => $listeUsersByPlaceDispo,
+            'listeUsersByTarif' => $listeUsersByTarif,
+            'listeUsersByAgrement' => $listeUsersByAgrement,
         ]);
     }
+
+    
 }
+
